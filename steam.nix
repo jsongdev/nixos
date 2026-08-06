284b715e517717cc86facc43cd3bb2c0e434b550
jsdev <song.johan.2007@gmail.com>
2026-08-06 02:08:00 -0700

    proton-ge fix

--- steam.nix
+++ steam.nix
@@ -10,8 +10,9 @@
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
   };
+  
+  programs.steam.extraCompatPackages = with pkgs; [
+    proton-ge-bin
+  ];
 }
