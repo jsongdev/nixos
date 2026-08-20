478a1a42a922a2ca8b70fea5ec2081f6b2ce0792
jsdev <song.johan.2007@gmail.com>
2026-08-20 01:18:39 -0700

    removed qt

--- configuration.nix
+++ configuration.nix
@@ -103,8 +103,6 @@ services.displayManager.dms-greeter = {
   };
 
   environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
-  qt.enable = true;
-  qt.platformTheme = "qt6ct";
   environment.systemPackages = with pkgs; [
     brightnessctl
     btop
