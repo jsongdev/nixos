b36d27eab6b2139fd80903a3e3ae7b34cb7d9470
jsdev <song.johan.2007@gmail.com>
2026-08-20 01:34:03 -0700

    qttheme

--- configuration.nix
+++ configuration.nix
@@ -101,7 +101,7 @@ services.displayManager.dms-greeter = {
     obs-studio.enable = true;
   };
   qt.enable = true;
-  qt.platformTheme = kde;
+  qt.platformTheme = "kde";
   environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
   environment.systemPackages = with pkgs; [
     brightnessctl
