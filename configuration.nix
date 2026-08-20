f290a7102573bebb20046a4b70cfd725973d427c
jsdev <song.johan.2007@gmail.com>
2026-08-20 01:19:30 -0700

    added qt

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,8 @@ services.displayManager.dms-greeter = {
   };
 
   environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+  qt.enable = true;
+  qt.platformTheme = "qt5ct";
   environment.systemPackages = with pkgs; [
     brightnessctl
     btop
