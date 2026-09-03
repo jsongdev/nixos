89601eb0fcc85efd5e569b599085d9dbf43591d4
jsdev <song.johan.2007@gmail.com>
2026-09-03 03:07:18 -0700

    changed timezone

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
 
   networking.hostName = "jsbox";
   networking.networkmanager.enable = true;
-  time.timeZone = "America/Vancouver";
+  time.timeZone = "America/Toronto";
   i18n.defaultLocale = "en_CA.UTF-8";
 
   services.xserver.enable = true;
