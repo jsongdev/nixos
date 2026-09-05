0b576210eb20f8cb78351c38ebce1c849968e0fe
jsdev <song.johan.2007@gmail.com>
2026-09-05 15:49:53 -0400

    added appimage-run, mono

--- dev.nix
+++ dev.nix
@@ -3,14 +3,15 @@
 {
 
   environment.systemPackages = with pkgs; [
+    appimage-run
     dotnet-sdk
     gcc
     love
+    mono
     neovim
     nodejs
     python314
     rustup
     vscode-fhs
-
   ];
 }
